d1e7bbccaefea8e3f46d760b3f84d6c01dd66e91
rasend <rasend@bk.ru>
2020-07-03 01:21:36 +0300

    Update activator.bat

--- activator.bat
+++ activator.bat
@@ -16,7 +16,7 @@ Set a=WNMTR-4C88C-JK8YV-HQ7T2-76DF9
 Set s=2F77B-TNFGY-69QQF-B8YKP-D69TJ 
 Set d=DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ  
 Set f=QFFDN-GRT3P-VKWWX-X7T3R-8B639
-endlocal
+
 
 
 echo Выберите ваше издание Windows 10
@@ -51,16 +51,13 @@ if (%win%)==(12) call :activ %s%
 if (%win%)==(13) call :activ %d% 
 if (%win%)==(14) call :activ %f% 
 
-call :activ %t% 
-
 :activ<key>
 cscript slmgr.vbs /ipk %1
 cls
 cscript slmgr.vbs /skms kms.lotro.cc
 cls
-For /F "qqq=" %%I In ('slmgr.vbs -ato') Do Set w=%%~I
+For /F "Delims=" %%I In ('slmgr.vbs -ato') Do Set V=%%~I
 cls
-Echo !w!
+Echo !V!
 exit
 goto :eof
-
