d845b821f8dd965df7224cdb0b96f81612522b97
rasend <rasend@bk.ru>
2020-07-03 02:30:24 +0300

    Update activator.bat

--- activator.bat
+++ activator.bat
@@ -2,20 +2,20 @@
 @chcp 1251
 
 setlocal EnableExtensions EnableDelayedExpansion
-Set q=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99        
-Set w=PVMJN-6DFY6-9CCP6-7BKTT-D3WVR  
-Set e=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH  
-Set r=3KHY7-WNT83-DGQKR-F7HPR-844BM 
-Set t=W269N-WFGWX-YVC9B-4J6C9-T83GX 
-Set y=MH37W-N47XK-V7XM9-C7227-GCQG9 
-Set u=NPPR9-FWDCX-D2C8J-H872K-2YT43 
-Set i=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 
-Set o=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 
-Set p=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ 
-Set a=WNMTR-4C88C-JK8YV-HQ7T2-76DF9
-Set s=2F77B-TNFGY-69QQF-B8YKP-D69TJ 
-Set d=DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ  
-Set f=QFFDN-GRT3P-VKWWX-X7T3R-8B639
+set arr[0]=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99        
+set arr[1]=PVMJN-6DFY6-9CCP6-7BKTT-D3WVR  
+set arr[2]=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH  
+set arr[3]=3KHY7-WNT83-DGQKR-F7HPR-844BM 
+set arr[4]=W269N-WFGWX-YVC9B-4J6C9-T83GX 
+set arr[5]=MH37W-N47XK-V7XM9-C7227-GCQG9 
+set arr[6]=NPPR9-FWDCX-D2C8J-H872K-2YT43 
+set arr[7]=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 
+set arr[8]=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 
+set arr[9]=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ 
+set arr[10]=WNMTR-4C88C-JK8YV-HQ7T2-76DF9
+set arr[11]=2F77B-TNFGY-69QQF-B8YKP-D69TJ 
+set arr[12]=DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ  
+set arr[13]=QFFDN-GRT3P-VKWWX-X7T3R-8B639
 
 echo Выберите ваше издание Windows 10
 echo  1)  Home/Core
@@ -33,26 +33,18 @@ echo  12) Enterprise 2015 LTSB N
 echo  13) Enterprise 2016 LTSB
 echo  14) Enterprise 2016 LTSB N
 Set /P win="Укажите цифру: "
+set /a "x=%win%-1"
 
-if (%win%)==(1) call :activ %q% 
-if (%win%)==(2) call :activ %w% 
-if (%win%)==(3) call :activ %e% 
-if (%win%)==(4) call :activ %r% 
-if (%win%)==(5) call :activ %t% 
-if (%win%)==(6) call :activ %y% 
-if (%win%)==(7) call :activ %u% 
-if (%win%)==(8) call :activ %i% 
-if (%win%)==(9) call :activ %o% 
-if (%win%)==(10) call :activ %p% 
-if (%win%)==(11) call :activ %a% 
-if (%win%)==(12) call :activ %s% 
-if (%win%)==(13) call :activ %d% 
-if (%win%)==(14) call :activ %f% 
+if defined arr[%x%] ( 
+   call :aviation %%arr[%x%]%% 
+   
+)
 
-:activ<key>
+:aviation<key>
 cscript slmgr.vbs /ipk %1
 cscript slmgr.vbs /skms kms.lotro.cc
 For /F "Delims=" %%I In ('slmgr.vbs -ato') Do Set V=%%~I
 Echo !V!
 exit
 goto :eof
+
